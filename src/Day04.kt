d1fa8fbb022b3c523bb03b3fc6d6a02f1e4f778a
jvegarodriguez <jvegarodriguez@newrelic.com>
2023-12-04 15:27:12 +0100

    day 4 part 2 ❌

--- src/Day04.kt
+++ src/Day04.kt
@@ -3,25 +3,28 @@ import kotlin.math.pow
 fun main() {
     val dayId = "Day04"
 
-    fun cardScore(numbers: String): Long {
+    fun cardMatches(numbers: String): Long {
         val (winingNumbersRaw, myNumbersRaw) = numbers.splitTrim("|")
-        val winingNumbers = " $winingNumbersRaw "
+        val winingNumbers =
+            winingNumbersRaw.splitTrim(" ").filter { it.isNotEmpty() && it.isNotBlank() }
+                .map { it.toInt() }
         val myNumbers = myNumbersRaw.splitTrim(" ").filter { it.isNotEmpty() && it.isNotBlank() }
-        val matches = myNumbers.count { winingNumbers.contains(" $it ") }
-        return 2.0.pow(matches - 1.0).let {
-            if (it < 1) {
-                0
-            } else {
-                it
-            }
-        }.toLong()
+            .map { it.toInt() }
+        return myNumbers.count { winingNumbers.contains(it) }.toLong()
     }
 
     fun part1(input: List<String>): Long {
         var total = 0L
         for (line in input) {
             val (cardId, numbers) = line.splitTrim(":")
-            val score = cardScore(numbers)
+            val matches = cardMatches(numbers)
+            val score = 2.0.pow(matches - 1.0).let {
+                if (it < 1) {
+                    0
+                } else {
+                    it
+                }
+            }.toLong()
             println("$cardId worth $score")
             total += score
         }
@@ -29,7 +32,58 @@ fun main() {
     }
 
 
-    fun part2(input: List<String>): Long = -2
+    fun part2(input: List<String>): Long {
+        var numberOfCards = 0L
+        val pendingCards = mutableMapOf<String, Int>()
+        val matchesByCard = mutableMapOf<String, Long>()
+        val biggestCard = input.size.toLong()
+
+        for (line in input) {
+            val (cardId, numbers) = line.splitTrim(":")
+            val matches = cardMatches(numbers)
+            matchesByCard[cardId] = matches;
+            if (matches > 0) {
+                val id = cardId.splitTrim(" ").last().toInt()
+                val longRange = id + 1..(id + matches).coerceAtMost(biggestCard)
+                println(cardId + " with $matches generates " + longRange)
+                longRange.forEach {
+                    val key = "Card $it"
+                    pendingCards.compute(key) { _, currValue ->
+                        currValue?.plus(1) ?: 1
+                    }
+                }
+            }
+
+            numberOfCards++
+        }
+        println("going to pending cards")
+        var targetCard = 2
+        while (pendingCards.isNotEmpty()) {
+            val key = "Card $targetCard"
+            val pending = pendingCards.remove(key)
+            println("post processing $key with $pending ops")
+            if (pending == null) {
+                targetCard++
+                continue
+            }
+            numberOfCards += pending
+            val matches = matchesByCard[key]
+            if (matches == null || matches == 0L) {
+                targetCard++
+                continue
+            }
+            (targetCard + 1..(targetCard + matches).coerceAtMost(biggestCard)).forEach {
+                val _key = "Card $it"
+                pendingCards.compute(_key) { _, currValue ->
+                    currValue?.plus(pending) ?: pending
+                }
+            }
+
+            targetCard++
+        }
+
+        return numberOfCards
+    }
 
 //  test if implementation meets criteria from the description, like:
     val testInput = readInput("${dayId}_test")
